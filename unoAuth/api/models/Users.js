d2b9d73d92eae20305f2b5a03d6efe25fd9dc296
Paul-michel <paulmichel.garzaro@ynov.com>
2018-11-06 12:53:28 +0100

    Auth with sails

---
+++ unoAuth/api/models/Users.js
@@ -0,0 +1,60 @@
+/*
+ * Users.js
+ */
+const bcrypt = require('bcrypt-nodejs');
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+    email: {
+      type: 'string',
+      required: true,
+      unique: true
+    },
+    pseudo: {
+      type: 'string',
+      required: true,
+      unique: true
+    },
+    password: {
+      type: 'string',
+      required: true
+    },
+    winNb:{
+      type:'number',
+    },
+    playedNb:{
+      type:'number'
+    },
+    winrate:{
+      type:'number'
+    }
+  },
+  customToJSON: function() {
+    return _.omit(this, ['password'])
+  },
+  beforeCreate: function(user, cb){
+    bcrypt.genSalt(10, function(err, salt){
+      bcrypt.hash(user.password, salt, null, function(err, hash){
+        if(err) return cb(err);
+        user.password = hash;
+        return cb();
+     });
+    })
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+      
+  },
+
+};
+
